72eb928d8d201b084a2149c32fd1b672ce0c61c4
Guilherme <goliva12@hotmail.com>
2024-10-07 15:52:34 -0300

    Processamento da edição/atualização de contatos

--- config/process.php
+++ config/process.php
@@ -40,7 +40,42 @@ if (!empty($data)) {
             echo "Erro: $error";
         }
 
+    } else if ($data["type"] === "edit") {
+
+        $name = $data["name"];
+        $phone = $data["phone"];
+        $observations = $data["observations"];
+        $id = $data["id"];
+
+        $query = "  UPDATE contacts
+                    SET name = :name,
+                    phone = :phone,
+                    observations = :observations
+                    WHERE id = :id";
+
+        $stmt = $conn->prepare($query);
+
+        $stmt->bindParam(":name", $name);
+        $stmt->bindParam(":phone", $phone);
+        $stmt->bindParam(":observations", $observations);
+        $stmt->bindParam(":id", $id);
+
+        try {
+
+            $stmt->execute();
+            $_SESSION["msg"] = "Contato Atualizado com sucesso!";
+
+        } catch (PDOException $e) {
+            //erro na conexao
+            $error = $e->getMessage();
+            echo "Erro: $error";
+        }
+
+
+
+
     }
+
     //Redirecionamento para a HOME
     header("Location:" . $BASE_URL . "../index.php");
     //Seleção de Dados
